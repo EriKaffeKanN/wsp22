ba4ccc8299d0e5ab338d48f416bedea63b633db5
EriKaffeKanN <errik.jag@gmail.com>
2022-04-17 19:21:26 +0200

    login, validation and authorization

--- app.rb
+++ app.rb
@@ -8,12 +8,61 @@ require_relative 'model'
 
 enable :sessions
 
+# --- Constants ---
+def dbPath
+    "db/reviewsplus.db"
+end
+
+# --- Misc ---
+before do
+    if request.path_info != "/users/new"
+        if request.path_info.include?("/new") && getUser == nil
+            session[:error] = "You need to log in to perform this action"
+            redirect("/error")
+        end
+    end
+end
+
+get "/error" do
+    "Error: #{session[:error]}"
+end
+
+# --- Standard routes ---
+
+post "/users/logout" do
+    session[:user_id] = nil
+    redirect("/")
+end
+
+post "/login" do
+    # TODO: VALIDATE
+    if authenticate_user(params[:login], params[:password], dbPath)
+        login_user(params[:login], dbPath)
+        redirect("/")
+    else
+        redirect("/users/login")
+    end
+end
+
+get "/users/login" do
+    slim(:"users/login", locals:{loginError:session[:loginError]})
+end
+
 post "/users" do
-    
+    if validate_user_registration(params[:username], params[:email], params[:password], params[:confirm], dbPath)
+        register_user(params[:username], params[:email], params[:password], dbPath)
+        redirect("/")
+    else
+        redirect("/users/new")
+    end
+end
+
+after "/users" do
+    session[:loginError] = nil
 end
 
 get "/users/new" do
-    slim(:"users/new", locals:{})
+    slim(:"users/new", locals:{loginError:session[:loginError]})
 end
 
 get "/users/" do
@@ -21,35 +70,42 @@ get "/users/" do
 end
 
 post "/categories/:category_id/:review_id/delete" do
-    db = connect_to_db("db/reviewsplus.db")
-    db.execute("DELETE FROM review WHERE id = ?", params["review_id"])
-    redirect("/categories/#{params["category_id"]}")
+    db = connect_to_db(dbPath)
+    # Authorize
+    ownerId = db.execute("SELECT author_id FROM review WHERE id = ?", params[:review_id])
+    moderatorIds = db.execute("SELECT mod_id FROM moderator_review_relation WHERE review_id = ?", params[:review_id])
+    if !authorize_user(ownerId, moderatorIds, dbPath)
+        session[:error] = "You do not have permission to perform this action"
+        redirect("/error")
+    end
+    db.execute("DELETE FROM review WHERE id = ?", params[:review_id])
+    redirect("/categories/#{params[:category_id]}")
 end
 
 post "/categories/:id/new" do
-    db = connect_to_db("db/reviewsplus.db")
-    title = params["review_title"]
-    body = params["review_body"]
-    rating = params["review_rating"]
-    db.execute("INSERT INTO review (title, body, rating, author_id, category_id) VALUES (?, ?, ?, ?, ?)", title, body, rating, 0, params["id"])
-    redirect("/categories/#{params["id"]}")
+    db = connect_to_db(dbPath)
+    title = params[:review_title]
+    body = params[:review_body]
+    rating = params[:review_rating]
+    db.execute("INSERT INTO review (title, body, rating, author_id, category_id) VALUES (?, ?, ?, ?, ?)", title, body, rating, 0, params[:id])
+    redirect("/categories/#{params[:id]}")
 end
 
 get "/categories/:id/new" do
-    db = connect_to_db("db/reviewsplus.db")
-    category = db.execute("SELECT * FROM category WHERE id = ?", params["id"])
-    slim(:"reviews/new", locals:{category:category[0]})
+    db = connect_to_db(dbPath)
+    category = db.execute("SELECT * FROM category WHERE id = ?", params[:id]).first
+    slim(:"reviews/new", locals:{category:category})
 end
 
 get "/categories/:category_id/:review_id" do
-    db = connect_to_db("db/reviewsplus.db")
-    review = db.execute("SELECT * FROM review WHERE id = ?", params["review_id"])
-    slim(:"reviews/display", locals:{review:review[0]})
+    db = connect_to_db(dbPath)
+    review = db.execute("SELECT * FROM review WHERE id = ?", params[:review_id]).first
+    slim(:"reviews/display", locals:{review:review})
 end
 
 post "/categories/new" do
-    db = connect_to_db("db/reviewsplus.db")
-    name = params["cat_name"]
+    db = connect_to_db(dbPath)
+    name = params[:cat_name]
     db.execute("INSERT INTO category (name) VALUES (?)", name)
     redirect("/categories")
 end
@@ -59,21 +115,21 @@ get "/categories/new" do
 end
 
 get "/categories/:id" do
-    db = connect_to_db("db/reviewsplus.db")
+    db = connect_to_db(dbPath)
     cat_id = params[:id]
-    category = db.execute("SELECT * FROM category WHERE id = ?", cat_id)
+    category = db.execute("SELECT * FROM category WHERE id = ?", cat_id).first
     reviews = db.execute("SELECT * FROM review WHERE category_id = ?", cat_id)
-    slim(:"reviews/list", locals:{category:category[0], reviews:reviews})
+    slim(:"reviews/list", locals:{category:category, reviews:reviews})
 end
 
 get "/categories" do
-    db = connect_to_db("db/reviewsplus.db")
+    db = connect_to_db(dbPath)
     categories = db.execute("SELECT * FROM category")
     p categories
     slim(:"categories/list", locals:{data:categories})
 end
 
 get "/" do
-    db = connect_to_db("db/reviewsplus.db")
+    db = connect_to_db(dbPath)
     slim(:index, locals:{})
 end
--- model.rb
+++ model.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 # Ruby
 
 def connect_to_db(path)
@@ -6,25 +8,108 @@ def connect_to_db(path)
     return db
 end
 
-def register_user(userName, email, password, dbPath)
-{
+# TODO: tillämpa funktionen
+def validate_user_registration(username, email, password, confirm, dbPath)
+    db = connect_to_db(dbPath)
+
+    nameAldreadyExists = !db.execute("SELECT * FROM user WHERE name = ?", username).empty?
+    if nameAldreadyExists
+        session[:loginError] = "The name already exists"
+        return false
+    end
+
+    # Email
+    emailCorrectlyFormatted = URI::MailTo::EMAIL_REGEXP.match?(email)
+    emailAlreadyExists = !db.execute("SELECT * FROM user WHERE email = ?", email).empty?
+    if !emailCorrectlyFormatted
+        session[:loginError] = "Use a real email adress"
+        return false
+    elsif emailAlreadyExists
+        session[:loginError] = "That email already exists"
+        return false
+    end
+    # Password
+    if password != confirm
+        session[:loginError] = "Passwords did not match each other"
+        return false
+    elsif password.length < 8
+        session[:loginError] = "Password needs to be 8 characters or more"
+        return false
+    elsif !string_contains_any?(password, "1234567890")
+        session[:loginError] = "Password needs to contain at least 1 numerical value"
+        return false
+    end
+    return true
+end
+
+def string_contains_any?(str, contains)
+    return (str.split('') & contains.split('')).any?
+end
+
+def authenticate_user(login, password, dbPath) # Login is email or username
+    db = connect_to_db(dbPath)
+
+    # Check if login is email or username
+    userAry = []
+    if URI::MailTo::EMAIL_REGEXP.match?(login)
+        userAry = db.execute("SELECT * FROM user WHERE email = ?", login)
+        userExists = !userAry.empty?
+        unless userExists
+            session[:loginError] = "That email is not registered"
+            return false
+        end
+    else
+        userAry = db.execute("SELECT * FROM user WHERE name = ?", login)
+        userExists = !userAry.empty?
+        unless userExists
+            session[:loginError] = "That user does not exist"
+            return false
+        end
+    end
+        
+    # Check if password is correct
+    user = userAry.first # Should never be empty if this part of the code is reached
+    pwDigest = user["pwdigest"]
+    pwDigest = BCrypt::Password.new(pwDigest)
+    unless pwDigest == password
+        session[:loginError] = "Incorrect password"
+        return false
+    end
+    return true
+end
+
+def login_user(login, dbPath)
+    db = connect_to_db(dbPath)
+    # Check if login is email or username
+    user = nil
+    if URI::MailTo::EMAIL_REGEXP.match?(login)
+        user = db.execute("SELECT * FROM user WHERE email = ?", login).first
+    else
+        user = db.execute("SELECT * FROM user WHERE name = ?", login).first
+    end
+    session[:user_id] = user["id"]
+end
+
+def register_user(username, email, password, dbPath)
     db = connect_to_db(dbPath)
     pwDigest = BCrypt::Password.create(password)
-    db.execute("INSERT INTO user (name, email, password) VALUES (?, ?, ?)", userName, email, pwDigest)
-}
+    db.execute("INSERT INTO user (name, email, pwdigest) VALUES (?, ?, ?)", username, email, pwDigest)
+    login_user(email, dbPath)
+end
 
-def authenticate(ownerId, reqPermissionLevel)
-    db = connect_to_db()
+def authorize_user(ownerId, moderatorIds, dbPath)
+    db = connect_to_db(dbPath)
 
     userId = session[:user_id]
-    userPermissionLevel = db.execute("SELECT permission FROM user WHERE user_id = ?", userId).first
+    userIsAdmin = db.execute("SELECT admin FROM user WHERE id = ?", userId).first == 1
     if userId == nil
         return false
     end
     
-    if userId == ownerId or userPermissionLevel >= reqPermissionLevel
+    if userId == ownerId or moderatorIds.include?(userId) or userIsAdmin
         return true
     end
+    return false
 end
 
 # Slim
